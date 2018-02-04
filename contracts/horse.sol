d23ef0c872ba3f455cbdf27c8858283b97e7f873
Anson <git@anson-cheung.com>
2018-02-04 23:28:58 +0800

    Add offering system

--- contracts/horse.sol
+++ contracts/horse.sol
@@ -3,7 +3,9 @@ contract horse {
     struct Pony {
         string name;
         uint256 locked_amount;
-        uint256 sale_price;
+        uint256 minimum_offer;
+        uint256 current_offer;
+        address current_offer_address;
         address owner;
         bool for_sale;
         
@@ -27,6 +29,9 @@ contract horse {
         require(msg.value >= 0.01 ether);
         pony[pony_counter].name = _name;
         pony[pony_counter].locked_amount = msg.value;
+        pony[pony_counter].minimum_offer = 0;
+        pony[pony_counter].current_offer = 0;
+        pony[pony_counter].current_offer_address = 0x0;
         pony[pony_counter].owner = msg.sender;
         pony[pony_counter].for_sale = false;
         pony[pony_counter].mane_1 = _mane_1;
@@ -54,6 +59,8 @@ contract horse {
         require(msg.sender == pony[pony_id].owner);
         pony[pony_id].name = '';
         pony[pony_id].locked_amount = 0;
+        pony[pony_counter].minimum_offer = 0;
+        pony[pony_counter].current_offer = 0;
         pony[pony_id].owner = 0x0;
         pony[pony_id].for_sale = false;
         pony[pony_id].mane_1 = 0;
@@ -70,17 +77,39 @@ contract horse {
         pony[pony_id].for_sale = sale;
     }
     
-    function set_price(uint8 pony_id, uint256 price) public {
+    function set_minimum_offer(uint8 pony_id, uint256 price) public {
         require(msg.sender == pony[pony_id].owner);
-        pony[pony_id].sale_price = price;
+        pony[pony_id].minimum_offer = price;
+        pony[pony_id].current_offer = 0;
     }
     
-    function buy(uint8 pony_id) public payable {
-        require(msg.value >= pony[pony_id].sale_price);
+    function offer(uint8 pony_id) public payable {
         require(pony[pony_id].for_sale == true);
-        var amount_to_send = msg.value;
-        var old_owner = pony[pony_id].owner;
-        pony[pony_id].owner = msg.sender;
-        old_owner.transfer(amount_to_send);
+        require(pony[pony_id].current_offer_address != msg.sender);
+        if (pony[pony_id].current_offer_address != 0x0) {
+            // If not the first bid, return the offer deposit to the last person
+            var amount_to_send = pony[pony_id].current_offer;
+            pony[pony_id].current_offer = 0;
+            pony[pony_id].current_offer_address.transfer(amount_to_send);
+        }
+        pony[pony_id].current_offer = msg.value;
+        pony[pony_id].current_offer_address = msg.sender;
+    }
+    
+    function increase_offer(uint8 pony_id, uint256 new_offer) public payable {
+        require(pony[pony_id].for_sale == true);
+        require(new_offer - pony[pony_id].current_offer == msg.value); // Increase in offer == ether sent
+        require(pony[pony_id].current_offer_address == msg.sender);
+        pony[pony_id].current_offer = msg.value;
+    }
+    
+    function accept_offer(uint8 pony_id) public {
+        require(msg.sender == pony[pony_id].owner);
+        require(pony[pony_id].current_offer_address != 0x0);
+        var amount_to_send = pony[pony_id].current_offer;
+        pony[pony_id].current_offer = 0;
+        pony[pony_id].current_offer_address = 0x0;
+        pony[pony_id].owner.transfer(amount_to_send);
+        pony[pony_id].owner = pony[pony_id].current_offer_address;
     }
 }
