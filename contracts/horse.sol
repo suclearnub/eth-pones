ca7b7d32ed22401101c58d3502b6dea544b00d6c
Anson <git@anson-cheung.com>
2018-02-07 21:48:57 +0800

    Fix bugs with where to send ether

--- contracts/horse.sol
+++ contracts/horse.sol
@@ -89,18 +89,26 @@ contract horse {
     
     function offer(uint8 pony_id) public payable {
         require(pony[pony_id].for_sale == true);
-        require(pony[pony_id].current_offer_address != msg.sender);
+        require(pony[pony_id].current_offer_address != msg.sender); // To increase own offer use the increase_offer() function.
+        require(pony[pony_id].owner != msg.sender); // The owner can't offer.
+        require(pony[pony_id].current_offer + 0.01 ether <= msg.value);
+        // ^^ The new offer being made must be of value current_offer + 0.01 ether or greater.
+        require(pony[pony_id].minimum_offer <= msg.value);
+        // ^^ The new offer must also be greater than the minimum offer.
         var ret = false;
         if (pony[pony_id].current_offer_address != 0x0) {
-            // If not the first bid, make the ret TRUE
+            // If not the first bid
             var amount_to_send = pony[pony_id].current_offer;
+            var old_bidder = pony[pony_id].current_offer_address;
             ret = true;
         }
-        // Regardless, update to the newest offer.
+        // Then update to the newest offer.
         pony[pony_id].current_offer = msg.value;
         pony[pony_id].current_offer_address = msg.sender;
         if (ret == true) {
-            pony[pony_id].current_offer_address.transfer(amount_to_send);
+            // If it was determined that it's not the first bid, then we
+            // should return the previous bidder's offer lock.
+            old_bidder.transfer(amount_to_send);
         }
     }
     
@@ -111,14 +119,18 @@ contract horse {
         pony[pony_id].current_offer = msg.value;
     }
     
-    function accept_offer(uint8 pony_id) public {
+    function accept_offer(uint8 pony_id, address intended) public {
         require(msg.sender == pony[pony_id].owner);
         require(pony[pony_id].current_offer_address != 0x0); // Reject if nobody's offered
+        require(pony[pony_id].current_offer_address == intended); 
+        // ^^^ This is to prevent offer sniping. If the current offer address is changed
+        // before this transaction goes through, it will revert.
         var amount_to_send = pony[pony_id].current_offer;
+        var send_to =  pony[pony_id].current_offer_address;
         pony[pony_id].current_offer = 0;
         pony[pony_id].current_offer_address = 0x0;
         pony[pony_id].owner = pony[pony_id].current_offer_address;
-        pony[pony_id].owner.transfer(amount_to_send);
+        send_to.transfer(amount_to_send);
     }
     
 }
